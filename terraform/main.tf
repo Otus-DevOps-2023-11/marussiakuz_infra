8832e62ee08d579a8186bf409c2fc1b0f2dba700
@marina_kuznetsova <m.kuznecova@webinnovations.ru>
2024-02-11 14:40:05 +0300

    terraform and providers versions updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 0.12"
 
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.94.0"
+      version = ">= 0.90"
     }
   }
 }
